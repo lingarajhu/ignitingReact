fbeeb583e8f6adbbc7507fc1024f3214356e85a3
lingarajhu <lingrajnie2@gmail.com>
2024-04-01 21:06:06 +0530

    Understanding the parcel bundler

---
+++ README.md
@@ -0,0 +1,19 @@
+## what parcel can do...
+
+- Hot Module Replacement
+- Minification
+- Caching
+- Image Optimization
+- File watching algorithm written in C++
+- Error handling
+- Creates Local server
+- creates HTTPS
+- Tree shaking -> remove unused code
+- Dev Build
+- Differential Bundling - support for older browser
+- Diagnostic
+- Consisting Hashing
+- Code splitting
+- Compress
+- Different dev and prod bundles
+- Bundling
--- script.js
+++ script.js
@@ -1,2 +1,27 @@
-import React from "/react";
-import ReactDOM from "/react-dom/client";
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+// * React.createElement() => object => ReactDOM.createElement => HTMLelement(remder)
+
+// * JSX = HTML like or XML like syntax
+// ! this syntax cannot be understand by JS engine so Babel takes the JSX and transpiles into JavaScript before it reaches the JS engine
+
+// ? JSX => transpile into React.createElement => React.createElement -> JS Object => HTMLelement(renders)
+
+const heading = (
+  <h1 id="heading" className="myClass">
+    Hi This heading from JSX
+  </h1>
+);
+
+// ! this is component composition
+const HeadingComponent = () => (
+  <div>
+    {heading}
+    <h1 id="heading2">This is functional component 🚀</h1>
+  </div>
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<HeadingComponent />);
